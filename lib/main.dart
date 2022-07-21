499d98b8218fedb51b0240c69e083cec8923269f
Lautaro Gonzalez <lautarogonzalez@CTL-DES-04.fibertel.com.ar>
2022-07-21 16:15:29 -0300

    Adding bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:curzo_avanzado_flutter_plat/places/bloc/place_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 import 'platzi_trips_cupertino.dart';
 
@@ -7,16 +9,16 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(),
-      //home: PlatziTrips()
-      home: const PlatziTripsCupertino(),
-    );
+    return BlocProvider(
+        bloc: PlaceBloc(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(),
+          //home: PlatziTrips()
+          home: const PlatziTripsCupertino(),
+        ));
   }
 }
 
